b01e77e17154be670e14c6578dd4cbbe935957b0
Eldorados <mjmcg1@hotmail.com>
2012-01-26 15:57:20 -0500

    Updated with RapidShare urls

--- plugin.video.icefilms/container_urls.py
+++ plugin.video.icefilms/container_urls.py
@@ -13,31 +13,31 @@ def get():
           containers['date'] = 'Dec 2011'
           
           #--- Database Meta Container ---# 
-          containers['db_url'] = 'http://www.2shared.com/file/kI9mnhlZ/video_cache.html'
+          containers['db_url'] = 'https://rapidshare.com/files/743649507/video_cache.zip'
           containers['db_size'] = 11
                     
           #--- Movie Meta Container ---# 
 
           #basic container        
-          containers['mv_covers_url'] = 'http://www.2shared.com/file/6nuG38Vn/movie_covers.html'
+          containers['mv_covers_url'] = 'https://rapidshare.com/files/2789189937/movie_covers.zip'
           containers['mv_cover_size'] = 265
           
-          containers['mv_backdrop_1_url'] = 'http://www.2shared.com/file/BhSTZsJS/movie_backdrops1.html'
-          containers['mv_backdrop_2_url'] = 'http://www.2shared.com/file/ctm3lQLI/movie_backdrops2.html'
-          containers['mv_backdrop_3_url'] = 'http://www.2shared.com/file/KGVP9scV/movie_backdrops3.html'
+          containers['mv_backdrop_1_url'] = 'https://rapidshare.com/files/111604620/movie_backdrops1.zip'
+          containers['mv_backdrop_2_url'] = 'https://rapidshare.com/files/2352819440/movie_backdrops2.zip'
+          containers['mv_backdrop_3_url'] = 'https://rapidshare.com/files/2337530913/movie_backdrops3.zip'
           containers['mv_backdrop_size'] = 2600
           
           #--- TV   Meta  Container ---#
 
           #basic container       
-          containers['tv_covers_url'] = 'http://www.2shared.com/file/6fPgfIjH/tv_covers.html'
+          containers['tv_covers_url'] = 'https://rapidshare.com/files/779111945/tv_covers.zip'
           containers['tv_cover_size'] = 530
 
-          containers['tv_banners_url'] = 'http://www.2shared.com/file/JV2mFfRx/tv_banners.html'
+          containers['tv_banners_url'] = 'https://rapidshare.com/files/60134521/tv_banners.zip'
           containers['tv_banners_size'] = 175
 
 
-          containers['tv_backdrop_url'] = 'http://www.2shared.com/file/de060K2m/tv_backdrops.html'
+          containers['tv_backdrop_url'] = 'https://rapidshare.com/files/1669815270/tv_backdrops.zip'
           containers['tv_backdrop_size'] = 832
           
           
