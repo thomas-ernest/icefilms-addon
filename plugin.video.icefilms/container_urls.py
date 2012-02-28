24e55a5dd072ca0e0fa1b286d6fb365889fe256b
Eldorados <mjmcg1@hotmail.com>
2012-02-28 12:37:54 -0500

    Updated links to use minus.com

--- plugin.video.icefilms/container_urls.py
+++ plugin.video.icefilms/container_urls.py
@@ -10,35 +10,39 @@ def get():
           containers = {} 
 
           #date updated
-          containers['date'] = 'Dec 2011'
+          containers['date'] = 'March 2012'
           
           #--- Database Meta Container ---# 
-          containers['db_url'] = 'https://rapidshare.com/files/743649507/video_cache.zip'
-          containers['db_size'] = 11
+          containers['db_url'] = 'http://minus.com/mhRcwPnWX'
+          containers['db_filename'] = 'video_cache.zip'
+          containers['db_size'] = 2
                     
           #--- Movie Meta Container ---# 
 
           #basic container        
-          containers['mv_covers_url'] = 'https://rapidshare.com/files/2789189937/movie_covers.zip'
-          containers['mv_cover_size'] = 265
+          containers['mv_covers_url'] = 'http://minus.com/mhRcwPnWX'
+          containers['mv_covers_filename'] = 'movie_covers.zip'
+          containers['mv_cover_size'] = 53
           
-          containers['mv_backdrop_1_url'] = 'https://rapidshare.com/files/111604620/movie_backdrops1.zip'
-          containers['mv_backdrop_2_url'] = 'https://rapidshare.com/files/2352819440/movie_backdrops2.zip'
-          containers['mv_backdrop_3_url'] = 'https://rapidshare.com/files/2337530913/movie_backdrops3.zip'
-          containers['mv_backdrop_size'] = 2600
+          containers['mv_backdrop_url'] = 'http://minus.com/mhRcwPnWX'
+          containers['mv_backdrop_filename'] = 'movie_backdrops.zip'
+          containers['mv_backdrop_size'] = 735
           
           #--- TV   Meta  Container ---#
 
           #basic container       
-          containers['tv_covers_url'] = 'https://rapidshare.com/files/779111945/tv_covers.zip'
-          containers['tv_cover_size'] = 530
+          containers['tv_covers_url'] = 'http://minus.com/mhRcwPnWX'
+          containers['tv_covers_filename'] = 'tv_covers.zip'
+          containers['tv_cover_size'] = 131
 
-          containers['tv_banners_url'] = 'https://rapidshare.com/files/60134521/tv_banners.zip'
-          containers['tv_banners_size'] = 175
+          containers['tv_banners_url'] = 'http://minus.com/mhRcwPnWX'
+          containers['tv_banners_filename'] = 'tv_banners.zip'
+          containers['tv_banners_size'] = 45
 
 
-          containers['tv_backdrop_url'] = 'https://rapidshare.com/files/1669815270/tv_backdrops.zip'
-          containers['tv_backdrop_size'] = 832
+          containers['tv_backdrop_url'] = 'http://minus.com/mhRcwPnWX'
+          containers['tv_backdrop_filename'] = 'tv_backdrops.zip'
+          containers['tv_backdrop_size'] = 210
           
           
           #additional container
