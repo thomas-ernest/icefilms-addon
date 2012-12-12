eaba5b37a0a67c93055cdf3150eb306af630c112
Eldorados <mjmcg1@hotmail.com>
2012-12-12 15:14:57 -0500

    regex update for bad url's returned on searches

--- plugin.video.icefilms/resources/lib/xgoogle/search.py
+++ plugin.video.icefilms/resources/lib/xgoogle/search.py
@@ -250,9 +250,9 @@ class GoogleSearch(object):
         title = ''.join(title_a.findAll(text=True))
         title = self._html_unescape(title)
         url = title_a['href']
-        match = re.match(r'/url\?q=(http[^&]+)&', url)
+        match = re.compile(r'url=(.+?)&rct').findall(url)[0]
         if match:
-            url = urllib.unquote(match.group(1))
+            url = urllib.unquote(match)
         return title, url
 
     def _extract_description(self, result):
