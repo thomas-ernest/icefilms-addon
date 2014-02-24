7b0be8523a4aebb5f8047a741a87f94074d6b474
Eldorados <eldorado.xbmc@gmail.com>
2014-02-23 20:25:48 -0500

    Removed separate urls, use only one setting

--- plugin.video.icefilms/container_urls.py
+++ plugin.video.icefilms/container_urls.py
@@ -11,36 +11,30 @@ def get():
 
           #date updated
           containers['date'] = 'May 2013'
+          containers['url'] = 'http://user.gosub.dk/eldorado/'
           
           #--- Database Meta Container ---# 
-          containers['db_url'] = 'http://user.gosub.dk/eldorado/'
           containers['db_filename'] = 'video_cache.zip'
           containers['db_size'] = 2
                     
           #--- Movie Meta Container ---# 
 
           #basic container        
-          containers['mv_covers_url'] = 'http://user.gosub.dk/eldorado/'
           containers['mv_covers_filename'] = 'movie_covers.zip'
           containers['mv_cover_size'] = 53
           
-          containers['mv_backdrop_url'] = 'http://user.gosub.dk/eldorado/'
           containers['mv_backdrop_filename'] = 'movie_backdrops.zip'
           containers['mv_backdrop_size'] = 735
           
           #--- TV   Meta  Container ---#
 
           #basic container       
-          containers['tv_covers_url'] = 'http://user.gosub.dk/eldorado/'
           containers['tv_covers_filename'] = 'tv_covers.zip'
           containers['tv_cover_size'] = 131
 
-          containers['tv_banners_url'] = 'http://user.gosub.dk/eldorado/'
           containers['tv_banners_filename'] = 'tv_banners.zip'
           containers['tv_banners_size'] = 45
 
-
-          containers['tv_backdrop_url'] = 'http://user.gosub.dk/eldorado/'
           containers['tv_backdrop_filename'] = 'tv_backdrops.zip'
           containers['tv_backdrop_size'] = 210
           
