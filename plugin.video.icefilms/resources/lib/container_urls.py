32891d5a1595cb4fff39f26584948618bc242761
Eldorados <eldorado.xbmc@gmail.com>
2014-06-09 15:13:06 -0400

    Update file sizes Start adding new mirror locations

--- plugin.video.icefilms/resources/lib/container_urls.py
+++ plugin.video.icefilms/resources/lib/container_urls.py
@@ -11,7 +11,10 @@ def get():
 
           #date updated
           containers['date'] = 'May 2013'
-          containers['url'] = 'http://user.gosub.dk/eldorado/'
+          
+          containers['url_tuxen'] = 'http://user.gosub.dk/eldorado/'
+          containers['url_mega'] = 'http://mega.co.nz/'
+          containers['url_firedrive'] = 'http://firedrive.com/'
           
           #--- Database Meta Container ---# 
           containers['db_filename'] = 'video_cache.zip'
@@ -21,22 +24,22 @@ def get():
 
           #basic container        
           containers['mv_covers_filename'] = 'movie_covers.zip'
-          containers['mv_cover_size'] = 53
+          containers['mv_cover_size'] = 89
           
           containers['mv_backdrop_filename'] = 'movie_backdrops.zip'
-          containers['mv_backdrop_size'] = 735
+          containers['mv_backdrop_size'] = 1100
           
           #--- TV   Meta  Container ---#
 
           #basic container       
           containers['tv_covers_filename'] = 'tv_covers.zip'
-          containers['tv_cover_size'] = 131
+          containers['tv_cover_size'] = 223
 
           containers['tv_banners_filename'] = 'tv_banners.zip'
-          containers['tv_banners_size'] = 45
+          containers['tv_banners_size'] = 94
 
           containers['tv_backdrop_filename'] = 'tv_backdrops.zip'
-          containers['tv_backdrop_size'] = 210
+          containers['tv_backdrop_size'] = 395
           
           
           #additional container
