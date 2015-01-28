059ee6640656a80859a51a932fca191847971c4f
Eldorados <eldorado.xbmc@gmail.com>
2015-01-28 14:26:17 -0500

    Fix BillionUploads resolver Remove dead resolvers

--- plugin.video.icefilms/resources/lib/resolvers.py
+++ plugin.video.icefilms/resources/lib/resolvers.py
@@ -379,7 +379,7 @@ def resolve_movreel(url):
         if link:
             return link.group(1)
         else:
-        	  raise Exception("Unable to find final link")
+            raise Exception("Unable to find final link")
 
     except Exception, e:
         addon.log_error('**** Movreel Error occured: %s' % e)
@@ -398,233 +398,62 @@ def resolve_billionuploads(url):
         dialog.update(0)
         
         addon.log('BillionUploads - Requesting GET URL: %s' % url)
-        cookie_file = os.path.join(cookie_path,'billionuploads.lwp')
         
-        cj = cookielib.LWPCookieJar()
-        if os.path.exists(cookie_file):
-            try: cj.load(cookie_file,True)
-            except: cj.save(cookie_file,True)
-        else: cj.save(cookie_file,True)
+        headers = {
+                   'Host': 'billionuploads.com'
+                }
         
-        normal = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-        headers = [
-            ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0'),
-            ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
-            ('Accept-Language', 'en-US,en;q=0.5'),
-            ('Accept-Encoding', ''),
-            ('DNT', '1'),
-            ('Connection', 'keep-alive'),
-            ('Pragma', 'no-cache'),
-            ('Cache-Control', 'no-cache')
-        ]
-        normal.addheaders = headers
-        class NoRedirection(urllib2.HTTPErrorProcessor):
-            # Stop Urllib2 from bypassing the 503 page.
-            def http_response(self, request, response):
-                code, msg, hdrs = response.code, response.msg, response.info()
-                return response
-            https_response = http_response
-        opener = urllib2.build_opener(NoRedirection, urllib2.HTTPCookieProcessor(cj))
-        opener.addheaders = normal.addheaders
-        response = opener.open(url).read()
-        decoded = re.search('(?i)var z="";var b="([^"]+?)"', response)
-        if decoded:
-            decoded = decoded.group(1)
-            z = []
-            for i in range(len(decoded)/2):
-                z.append(int(decoded[i*2:i*2+2],16))
-            decoded = ''.join(map(unichr, z))
-            incapurl = re.search('(?i)"GET","(/_Incapsula_Resource[^"]+?)"', decoded)
-            if incapurl:
-                incapurl = 'http://billionuploads.com'+incapurl.group(1)
-                opener.open(incapurl)
-                cj.save(cookie_file,True)
-                response = opener.open(url).read()
-                
-        captcha = re.search('(?i)<iframe src="(/_Incapsula_Resource[^"]+?)"', response)
-        if captcha:
-            captcha = 'http://billionuploads.com'+captcha.group(1)
-            opener.addheaders.append(('Referer', url))
-            response = opener.open(captcha).read()
-            formurl = 'http://billionuploads.com'+re.search('(?i)<form action="(/_Incapsula_Resource[^"]+?)"', response).group(1)
-            resource = re.search('(?i)src=" (/_Incapsula_Resource[^"]+?)"', response)
-            if resource:
-                import random
-                resourceurl = 'http://billionuploads.com'+resource.group(1) + str(random.random())
-                opener.open(resourceurl)
-            recaptcha = re.search('(?i)<script type="text/javascript" src="(https://www.google.com/recaptcha/api[^"]+?)"', response)
-            if recaptcha:
-                response = opener.open(recaptcha.group(1)).read()
-                challenge = re.search('''(?i)challenge : '([^']+?)',''', response)
-                if challenge:
-                    challenge = challenge.group(1)
-                    captchaimg = 'https://www.google.com/recaptcha/api/image?c=' + challenge
-                    img = xbmcgui.ControlImage(450,15,400,130,captchaimg)
-                    wdlg = xbmcgui.WindowDialog()
-                    wdlg.addControl(img)
-                    wdlg.show()
-                    
-                    xbmc.sleep(3000)
-                    
-                    kb = xbmc.Keyboard('', 'Please enter the text in the image', False)
-                    kb.doModal()
-                    capcode = kb.getText()
-                    if (kb.isConfirmed()):
-                        userInput = kb.getText()
-                        if userInput != '': capcode = kb.getText()
-                        elif userInput == '':
-                            logerror('BillionUploads - Image-Text not entered')
-                            xbmc.executebuiltin("XBMC.Notification(Image-Text not entered.,BillionUploads,2000)")              
-                            return None
-                    else: return None
-                    wdlg.close()
-                    captchadata = {}
-                    captchadata['recaptcha_challenge_field'] = challenge
-                    captchadata['recaptcha_response_field'] = capcode
-                    opener.addheaders = headers
-                    opener.addheaders.append(('Referer', captcha))
-                    resultcaptcha = opener.open(formurl,urllib.urlencode(captchadata)).info()
-                    opener.addheaders = headers
-                    response = opener.open(url).read()
-                    
-        ga = re.search('(?i)"text/javascript" src="(/ga[^"]+?)"', response)
-        if ga:
-            jsurl = 'http://billionuploads.com'+ga.group(1)
-            p  = "p=%7B%22appName%22%3A%22Netscape%22%2C%22platform%22%3A%22Win32%22%2C%22cookies%22%3A1%2C%22syslang%22%3A%22en-US%22"
-            p += "%2C%22userlang%22%3A%22en-US%22%2C%22cpu%22%3A%22WindowsNT6.1%3BWOW64%22%2C%22productSub%22%3A%2220100101%22%7D"
-            opener.open(jsurl, p)
-            response = opener.open(url).read()
-#         pid = re.search('(?i)PID=([^"]+?)"', response)
-#         if pid:
-#             normal.addheaders += [('Cookie','D_UID='+pid.group(1)+';')]
-#             opener.addheaders = normal.addheaders
-        if re.search('(?i)url=/distil_r_drop.html', response) and filename:
-            url += '/' + filename
-            response = normal.open(url).read()
-        jschl=re.compile('name="jschl_vc" value="(.+?)"/>').findall(response)
-        if jschl:
-            jschl = jschl[0]    
-            maths=re.compile('value = (.+?);').findall(response)[0].replace('(','').replace(')','')
-            domain_url = re.compile('(https?://.+?/)').findall(url)[0]
-            domain = re.compile('https?://(.+?)/').findall(domain_url)[0]
-            final= normal.open(domain_url+'cdn-cgi/l/chk_jschl?jschl_vc=%s&jschl_answer=%s'%(jschl,eval(maths)+len(domain))).read()
-            html = normal.open(url).read()
-        else: html = response
+        tries = 0
+        MAX_TRIES = 3
         
-        if dialog.iscanceled(): return None
-        dialog.update(25)
-        
-        #Check page for any error msgs
-        if re.search('This server is in maintenance mode', html):
-            addon.log_error('***** BillionUploads - Site reported maintenance mode')
-            raise Exception('File is currently unavailable on the host')
+        r = re.search('[^/]+(?=/$|$)', url)
+        if r:
+            url = 'http://www.billionuploads.com/%s' % r.group(0)
 
-        # Check for file not found
-        if re.search('File Not Found', html):
-            addon.log_error('***** BillionUploads - File Not Found')
-            raise Exception('File Not Found - Likely Deleted')  
+        def __incapsala_decode(s):
+            return s.decode('hex')          
+
+        while tries < MAX_TRIES:
+            html = net.http_GET(url, headers=headers).content
+            dialog.update(50)
+        
+            match=re.search('var\s+b\s*=\s*"([^"]+)', html)
+            if match:
+                html = __incapsala_decode(match.group(1))
+                match = re.search(',\s*"(/_Incapsula[^"]+)', html)
+                incap_url = 'http://www.billionuploads.com' + match.group(1)
+                net.http_GET(incap_url, headers=headers) # Don't need content, just the cookies
+            else:
+                # Even though a captcha can be returned, it seems not to be required if you just re-request the page
+                match = re.search('iframe\s+src="(/_Incapsula[^"]+)', html)
+                if match:
+                    captcha_url = 'http://www.billionuploads.com' + urllib.quote(match.group(1))
+                    html = net.http_GET(captcha_url, headers=headers).content
+                else:
+                    # not a Incapsula or a Captcha, so probably the landing page
+                    break
+            
+            tries = tries + 1
+        else:
+            raise Exception('Tries Ran Out')
+        
+        if re.search('>\s*File Not Found\s*<', html, re.I):
+            raise Exception('File Not Found/Removed')
 
         data = {}
-        r = re.findall(r'type="hidden" name="(.+?)" value="(.*?)">', html)
+        r = re.findall(r'type="hidden"\s+name="(.+?)"\s+value="(.*?)"', html)
         for name, value in r: data[name] = value
-        
-        if dialog.iscanceled(): return None
-        
-        captchaimg = re.search('<img src="((?:http://|www\.)?BillionUploads.com/captchas/.+?)"', html)            
-        if captchaimg:
+        data['method_free']='Download or watch'
 
-            img = xbmcgui.ControlImage(550,15,240,100,captchaimg.group(1))
-            wdlg = xbmcgui.WindowDialog()
-            wdlg.addControl(img)
-            wdlg.show()
-            
-            kb = xbmc.Keyboard('', 'Please enter the text in the image', False)
-            kb.doModal()
-            capcode = kb.getText()
-            if (kb.isConfirmed()):
-                userInput = kb.getText()
-                if userInput != '': capcode = kb.getText()
-                elif userInput == '':
-                    showpopup('BillionUploads','[B]You must enter the text from the image to access video[/B]',5000, elogo)
-                    return None
-            else: return None
-            wdlg.close()
-            
-            data.update({'code':capcode})
+        html = net.http_POST(url, form_data = data, headers = headers).content
         
-        if dialog.iscanceled(): return None
-        dialog.update(50)
-        
-        data.update({'submit_btn':''})
-        enc_input = re.compile('decodeURIComponent\("(.+?)"\)').findall(html)
-        if enc_input:
-            dec_input = urllib2.unquote(enc_input[0])
-            r = re.findall(r'type="hidden" name="(.+?)" value="(.*?)">', dec_input)
-            for name, value in r:
-                data[name] = value
-        extradata = re.compile("append\(\$\(document.createElement\('input'\)\).attr\('type','hidden'\).attr\('name','(.*?)'\).val\((.*?)\)").findall(html)
-        if extradata:
-            for attr, val in extradata:
-                if 'source="self"' in val:
-                    val = re.compile('<textarea[^>]*?source="self"[^>]*?>([^<]*?)<').findall(html)[0]
-                data[attr] = val.strip("'")
-        r = re.findall("""'input\[name="([^"]+?)"\]'\)\.remove\(\)""", html)
-        
-        for name in r: del data[name]
-        
-        normal.addheaders.append(('Referer', url))
-        html = normal.open(url, urllib.urlencode(data)).read()
-        cj.save(cookie_file,True)
-        
-        if dialog.iscanceled(): return None
-        dialog.update(75)
-        
-        def custom_range(start, end, step):
-            while start <= end:
-                yield start
-                start += step
-
-        def checkwmv(e):
-            s = ""
-            i=[]
-            u=[[65,91],[97,123],[48,58],[43,44],[47,48]]
-            for z in range(0, len(u)):
-                for n in range(u[z][0],u[z][1]):
-                    i.append(chr(n))
-            t = {}
-            for n in range(0, 64): t[i[n]]=n
-            for n in custom_range(0, len(e), 72):
-                a=0
-                h=e[n:n+72]
-                c=0
-                for l in range(0, len(h)):            
-                    f = t.get(h[l], 'undefined')
-                    if f == 'undefined': continue
-                    a = (a<<6) + f
-                    c = c + 6
-                    while c >= 8:
-                        c = c - 8
-                        s = s + chr( (a >> c) % 256 )
-            return s
-
-        dll = re.compile('<input type="hidden" id="dl" value="(.+?)">').findall(html)
-        if dll:
-            dl = dll[0].split('GvaZu')[1]
-            dl = checkwmv(dl);
-            dl = checkwmv(dl);
+        dialog.update(100)
+        r = re.search(r'class="[^"]*download"\s+href="([^"]+)', html)
+        if r:
+            return r.group(1)
         else:
-            alt = re.compile('<source src="([^"]+?)"').findall(html)
-            if alt:
-                dl = alt[0]
-            else:
-                addon.log_error('***** BillionUploads - No Video File Found')
-                raise Exception('Unable to resolve - No Video File Found')  
-        
-        if dialog.iscanceled(): return None
-        dialog.update(100)                    
-
-        return dl
-        
+            raise Exception('Unable to locate file link')
+    
     except Exception, e:
         addon.log_error('**** BillionUploads Error occured: %s' % e)
         raise
@@ -744,53 +573,6 @@ def resolve_megarelease(url):
         
     except Exception, e:
         addon.log_error('**** MegaRelease Error occured: %s' % e)
-        raise
-    finally:
-        dialog.close()
-
-
-def resolve_lemupload(url):
-
-    try:
-        
-        #Show dialog box so user knows something is happening
-        dialog = xbmcgui.DialogProgress()
-        dialog.create('Resolving', 'Resolving LemUploads Link...')
-        dialog.update(0)
-        
-        addon.log('LemUploads - Requesting GET URL: %s' % url)
-        html = net.http_GET(url).content
-
-        dialog.update(50)
-        
-        #Check page for any error msgs
-        if re.search('This server is in maintenance mode', html):
-            addon.log_error('***** LemUploads - Site reported maintenance mode')
-            raise Exception('File is currently unavailable on the host')
-        if re.search('<b>File Not Found</b>', html):
-            addon.log_error('***** LemUpload - File not found')
-            raise Exception('File has been deleted')
-
-        filename = re.search('<h2>(.+?)</h2>', html).group(1)
-        extension = re.search('(\.[^\.]*$)', filename).group(1)
-        guid = re.search('http://lemuploads.com/(.+)$', url).group(1)
-        
-        vid_embed_url = 'http://lemuploads.com/vidembed-%s%s' % (guid, extension)
-        
-        request = urllib2.Request(vid_embed_url)
-        request.add_header('User-Agent', USER_AGENT)
-        request.add_header('Accept', ACCEPT)
-        request.add_header('Referer', url)
-        response = urllib2.urlopen(request)
-        redirect_url = re.search('(http://.+?)video', response.geturl()).group(1)
-        download_link = redirect_url + filename
-        
-        dialog.update(100)
-
-        return download_link
-        
-    except Exception, e:
-        addon.log_error('**** LemUploads Error occured: %s' % e)
         raise
     finally:
         dialog.close()
@@ -1026,53 +808,6 @@ def resolve_donevideo(url):
         dialog.close()
 
 
-def resolve_pandaplanet(url):
-
-    try:
-        
-        #Show dialog box so user knows something is happening
-        dialog = xbmcgui.DialogProgress()
-        dialog.create('Resolving', 'Resolving PandaPlanet Link...')
-        dialog.update(0)
-        
-        addon.log('PandaPlanet - Requesting GET URL: %s' % url)
-        html = net.http_GET(url).content
-
-        dialog.update(50)
-        
-        #Check page for any error msgs
-        if re.search('This server is in maintenance mode', html):
-            addon.log_error('***** PandaPlanet - Site reported maintenance mode')
-            raise Exception('File is currently unavailable on the host')
-        if re.search('<b>File Not Found</b>', html):
-            addon.log_error('***** LemUpload - File not found')
-            raise Exception('File has been deleted')
-
-        filename = re.search('<td class="dofir" title="(.+?)" style="color:.+">(.+?)</td>', html).group(1)
-        extension = re.search('(\.[^\.]*$)', filename).group(1)
-        guid = re.search('http://pandapla.net/(.+)$', url).group(1)
-        
-        vid_embed_url = 'http://pandapla.net/vidembed-%s%s' % (guid, extension)
-        
-        request = urllib2.Request(vid_embed_url)
-        request.add_header('User-Agent', USER_AGENT)
-        request.add_header('Accept', ACCEPT)
-        request.add_header('Referer', url)
-        response = urllib2.urlopen(request)
-        redirect_url = re.search('(http://.+?)video', response.geturl()).group(1)
-        download_link = redirect_url + filename
-        
-        dialog.update(100)
-
-        return download_link
-        
-    except Exception, e:
-        addon.log_error('**** PandaPlanet Error occured: %s' % e)
-        raise
-    finally:
-        dialog.close()
-
-
 def resolve_360gig(url):
 
     try:
@@ -1155,3 +890,22 @@ def resolve_360gig(url):
         raise
     finally:
         dialog.close()
+
+
+def SHARED2_HANDLER(url):
+
+    html = net.http_GET(url).content
+
+    #Check if a download limit msg is showing
+    if re.search('Your free download limit is over.', html):
+      wait_time = re.search('<span id="timeToWait">(.+?)</span>', html).group(1)
+      Notify('big','2Shared Download Limit Exceeded','You have reached your download limit', '', '', 'You must wait ' + wait_time + ' to try again' )
+      return None
+
+    #If no download limit msg lets grab link, must post to it first for download to activate
+    else:
+      d3fid = re.search('<input type="hidden" name="d3fid" value="(.+?)">', html).group(1)
+      d3link = re.search('<input type="hidden" name="d3link" value="(.+?)">', html).group(1)
+      data = {'d3fid': d3fid, 'd3link': d3link}
+      html = net.http_POST(url, data).content
+      return d3link
